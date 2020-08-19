a400e1a402ceb0c9845b94ebbcf8a8f3c3cc56fb
ahsanv101 <ahsansyedv101@gmail.com>
2020-08-19 15:49:41 +0500

    api hide

--- Mern App/config/keys.js
+++ Mern App/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mongoURI: "mongodb://ahsanv101:123abc@ds119049.mlab.com:19049/resume-sys",
+  mongoURI: "mongo-url",
   secretOrKey: "secret",
-  CLOUDINARY_API: "712615274767759",
-  CLOUDINARY_SECRET: "cMi1MGO5ExqF-8PTPCWSivVlJEA"
+  CLOUDINARY_API: "cloudinary-api",
+  CLOUDINARY_SECRET: "cloudinary-secret"
 };
